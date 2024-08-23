764cc60b738aaa192a94f94b3ca0c172c0ecf0b1
Pier228 <artem.buxanenko@gmail.com>
2024-08-23 13:56:38 +0300

    init

---
+++ src/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { ThrottlerModule } from '@nestjs/throttler';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000,
+        limit: 60,
+      },
+    ]),
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
